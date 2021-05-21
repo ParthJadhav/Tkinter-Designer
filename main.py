569bf001672cac065aa65be4c8f2e027fc2a466d
Parth Jadhav <jadhavparth99@gmail.com>
2021-05-21 11:03:08 +0530

    improved functions and added features

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import PIL.ImageDraw
 import requests
-import pandas as pd
 from colormap import rgb2hex
 import PySimpleGUI as sg
 import tkmacosx
 import tkinter.ttk as ttk
 # from window import Figma_window
 
-code = open("code.txt", 'a')
+os.mkdir("./generated_code/")
+
 lines = []
-
-from tkinter import *
-window = Tk()
-
-def btnClicked():
-    print("hey")
-
-
-lines.extend(['from tkinter import *','window = Tk()\n','def btnClicked():','    print("hey")'])
+lines.extend(['from tkinter import *','window = Tk()','def btnClicked():\n','    print("Button Clicked")\n'])
 
 def get_color(element):
+    ''' Get's the element as input and checks for it's RGB color and converts and returns it's HEX COLOR. (STRING)'''
     el_r = element["fills"][0]["color"]['r'] * 255
     el_g = element["fills"][0]["color"]['g'] * 255
     el_b = element["fills"][0]["color"]['b'] * 255
@@ -27,16 +19,19 @@ def get_color(element):
     return hex
 
 def get_cordinates(element):
+    ''' Get's the element as input and returns it's coordinates in X, Y. (INT)'''
     x = int(element["absoluteBoundingBox"]["x"])
     y = int(element["absoluteBoundingBox"]["y"])
     return x,y
 
 def get_dimensions(element):
+    ''' Get's the element as input and returns it's dimensions in width, height. (INT)'''
     height = int(element["absoluteBoundingBox"]["height"])
     width = int(element["absoluteBoundingBox"]["width"])
     return width,height
 
 def get_text_properties(element):
+    ''' Get's the element as input and returns it's font and fontSize in String.'''
     font = element["style"]["fontPostScriptName"]
     fontSize = element["style"]["fontSize"]
     return font,fontSize
@@ -46,10 +41,8 @@ token = "189541-e5791cc9-4619-411e-b4b1-6b6f7d285d68"
 fileId = "8K2eByUz9tLasBT5sOFSzJ"
 response = requests.get("https://api.figma.com/v1/files/8K2eByUz9tLasBT5sOFSzJ", headers={"X-FIGMA-TOKEN": token})
 
-with open("js.json",'w') as js:
-    js.write(response.text)
-
-data = pd.read_json("js.json")
+data = response.json()
+print(data)
 
 ####################### Accesing Window Properties #######################
 fig_window = data["document"]["children"][0]['children'][0]
@@ -68,47 +61,40 @@ window_bg_hex = rgb2hex(round(window_color_r), round(window_color_g), round(wind
 
 ####################### Creating Window #######################
 
-window.geometry(f"{window_width}x{window_height}")
-window.configure(bg=window_bg_hex)
-canvas = Canvas(window,bg=window_bg_hex,height=window_height,width=window_width,bd=0, highlightthickness=0,relief="ridge")
-canvas.pack()
-lines.extend([f'\nwindow.geometry("{window_width}x{window_height}")',f'window.configure(bg="{window_bg_hex}")',f'canvas = Canvas(window,bg="{window_bg_hex}",height={window_height},width={window_width},bd=0, highlightthickness=0,relief="ridge")','canvas.pack()'])
+lines.extend([f'\nwindow.geometry("{window_width}x{window_height}")',f'window.configure(bg="{window_bg_hex}")',f'canvas = Canvas(window,bg="{window_bg_hex}",height={window_height},width={window_width},bd=0, highlightthickness=0,relief="ridge")','canvas.place(x=0,y=0)\n'])
 
 ####################### Getting Elements inside Window #######################
 window_elements = fig_window["children"]
 
 btn_count = 0
+text_entry_count = 0
+
 for element in window_elements:
 
     if element["name"] == 'Rectangle':
-        rec_width, rec_height = get_dimensions(element)
+        width, height = get_dimensions(element)
         x, y = get_cordinates(element)
         element_color = get_color(element)
-
-        canvas.create_rectangle(x, y, x + rec_width, y + rec_height, fill=element_color)
-
-        lines.extend([f'\ncanvas.create_rectangle({x}, {y}, {x} + {rec_width}, {y} + {rec_height}, fill="{element_color}")'])
+        # canvas.create_rectangle(x, y, x + width, y + height, fill=element_color)
+        lines.extend([f'\ncanvas.create_rectangle({x}, {y}, {x} + {width}, {y} + {height}, fill="{element_color}")\n'])
 
     elif element["name"] == 'Button':
-
-        rec_width, rec_height = get_dimensions(element)
+        width, height = get_dimensions(element)
         x, y = get_cordinates(element)
         element_color = get_color(element["children"][0])
         btn_text = element["children"][len(element["children"]) - 1]["characters"]
-        btn_id = element["id"]
-        response = requests.get(f"https://api.figma.com/v1/images/8K2eByUz9tLasBT5sOFSzJ?ids={btn_id}",
-                                headers={"X-FIGMA-TOKEN": "189543-60e9c4f6-7b8c-4fc1-b682-586c2fb8a0e7"})
-        res = requests.get(response.json()["images"][btn_id])
+        item_id = element["id"]
 
-        with open(f"img{btn_count}.png", "wb") as file:
+        response = requests.get(f"https://api.figma.com/v1/images/8K2eByUz9tLasBT5sOFSzJ?ids={item_id}", headers={"X-FIGMA-TOKEN": "189543-60e9c4f6-7b8c-4fc1-b682-586c2fb8a0e7"})
+        res = requests.get(response.json()["images"][item_id])
+        with open(f"./generated_code/img{btn_count}.png", "wb") as file:
             file.write(res.content)
 
-        img = PhotoImage(file=f"img{btn_count}.png")
+        # img = PhotoImage(file=f"img{btn_count}.png")
+        # b = Button(image=img, borderwidth=0, highlightthickness=0, command=btnClicked, relief="flat")
+        # b.place(x=x, y=y, width=width, height=height)
 
-        b = Button(image=img, borderwidth=0, highlightthickness=0, command=btnClicked, relief="flat")
-        b.place(x=x, y=y, width=rec_width, height=rec_height)
-
-        lines.extend([f'\nimg{btn_count} = PhotoImage(file=f"img{btn_count}.png")',f'b{btn_count} = Button(image=img{btn_count}, borderwidth=0, highlightthickness=0, command=btnClicked, relief="flat")',f'b{btn_count}.place(x={x}, y={y}, width={rec_width}, height={rec_height})\n'])
+        lines.extend([f'img{btn_count} = PhotoImage(file=f"img{btn_count}.png")',f'b{btn_count} = Button(image=img{btn_count}, borderwidth=0, highlightthickness=0, command=btnClicked, relief="flat")',f'b{btn_count}.place(x={x}, y={y}, width={width}, height={height})\n'])
         btn_count += 1
 
     elif element["type"] == 'TEXT':
@@ -118,10 +104,35 @@ for element in window_elements:
         color = get_color(element)
         font,fontSize = get_text_properties(element)
         x,y = x + (width/2), y + (height/2)
-        canvas.create_text(x,y,text=text,fill=color,font=(font,int(fontSize)))
-        lines.extend([f'canvas.create_text({x},{y},text="{text}",fill="{color}",font=("{font}",int({fontSize})))'])
+        # canvas.create_text(x,y,text=text,fill=color,font=(font,int(fontSize)))
+        lines.extend([f'canvas.create_text({x},{y},text="{text}",fill="{color}",font=("{font}",int({fontSize})))\n'])
 
-# for i in lines:
-#     print(i)
+    elif element["name"] == 'TextBox':
+        width, height = get_dimensions(element)
+        x, y = get_cordinates(element)
+        bg = get_color(element)
+        #entry = Entry(bd=0,bg=bg,highlightthickness=0)
+        # entry.place(x=x,y=y,width=width,height=height)
+        lines.extend([f'entry{text_entry_count} = Entry(bd=0,bg="{bg}",highlightthickness=0)',f'entry{text_entry_count}.place(x={x},y={y},width={width},height={height})\n'])
 
-window.mainloop()
+    elif element["name"] == "Background":
+        width, height = get_dimensions(element)
+        x, y = get_cordinates(element)
+        x, y = x + (width / 2), y + (height / 2)
+        item_id = element["id"]
+
+        response = requests.get(f"https://api.figma.com/v1/images/8K2eByUz9tLasBT5sOFSzJ?ids={item_id}&use_absolute_bounds=true",
+                                headers={"X-FIGMA-TOKEN": "189543-60e9c4f6-7b8c-4fc1-b682-586c2fb8a0e7"})
+        res = requests.get(response.json()["images"][item_id])
+        with open(f"./generated_code/background.png", "wb") as file:
+            file.write(res.content)
+
+        # img = PhotoImage(file=f"background.png")
+        # background = canvas.create_image(x,y,image=img)
+        lines.extend(['background_img = PhotoImage(file=f"background.png")',f'background = canvas.create_image({x},{y},image=background_img)\n'])
+
+lines.extend(['window.mainloop()'])
+final_code = [line + "\n" for line in lines]
+
+with open("./generated_code/window.py", 'w') as py_file:
+    py_file.writelines(final_code)
