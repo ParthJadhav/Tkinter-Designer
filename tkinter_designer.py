8a77ad5d2925429d368af637b81f0dfd90577154
Hunter Logan <hunter.gray.logan@gmail.com>
2021-06-02 02:51:28 -0400

    Overwrite output path instead of append

--- tkinter_designer.py
+++ tkinter_designer.py
@@ -23,6 +23,7 @@ def select_path(event):
     global path_to_save
     # window.withdraw()
     path_to_save = filedialog.askdirectory()
+    path_entry.delete(0, END)
     path_entry.insert(0, path_to_save)
     # window.deiconify()
 
