33e38ec88a7c185096ee6ea7cd43e35e566c462f
Parth Jadhav <jadhavparth99@gmail.com>
2021-06-16 18:51:49 +0530

    Added new features
    - Buttons now support Text
    - Now the output path can be typed

--- backend.py
+++ backend.py
@@ -2,18 +2,17 @@ import requests
 import os
 from tkinter import messagebox
 
-def generate_code(token, link, output_path):
 
+def generate_code(token, link, output_path):
     def get_color(element):
         # Returns HEX form of element RGB color (str)
-        el_r = element["fills"][0]["color"]['r'] * 255
-        el_g = element["fills"][0]["color"]['g'] * 255
-        el_b = element["fills"][0]["color"]['b'] * 255
+        el_r = element["fills"][0]["color"]["r"] * 255
+        el_g = element["fills"][0]["color"]["g"] * 255
+        el_b = element["fills"][0]["color"]["b"] * 255
 
-        hex_code = ('#%02x%02x%02x' % (round(el_r), round(el_g), round(el_b)))
+        hex_code = "#%02x%02x%02x" % (round(el_r), round(el_g), round(el_b))
 
         return hex_code
-
 
     def get_coordinates(element):
         # Returns element coordinates as x (int) and y (int)
@@ -22,14 +21,12 @@ def generate_code(token, link, output_path):
 
         return x, y
 
-
     def get_dimensions(element):
         # Return element dimensions as width (int) and height (int)
         height = int(element["absoluteBoundingBox"]["height"])
         width = int(element["absoluteBoundingBox"]["width"])
 
         return width, height
-
 
     def get_text_properties(element):
         # Return element font and fontSize (str)
@@ -38,17 +35,19 @@ def generate_code(token, link, output_path):
 
         return font, fontSize
 
-
     global fig_window, response
 
     generated_dir = output_path + "/generated_code/"
 
     lines = []
-    lines.extend(['from tkinter import *\n\n',
-                  'def btn_clicked():',
-                  '    print("Button Clicked")\n\n\n'
-                  'window = Tk()'])
-
+    lines.extend(
+        [
+            "from tkinter import *\n\n",
+            "import tkinter.font as font",
+            "def btn_clicked():",
+            '    print("Button Clicked")\n\n\n' "window = Tk()",
+        ]
+    )
 
     # Getting File Data
 
@@ -62,7 +61,6 @@ def generate_code(token, link, output_path):
         except ValueError:
             return ""
 
-
     token = token.strip()
     file_url = link.strip()
 
@@ -71,20 +69,20 @@ def generate_code(token, link, output_path):
     try:
         response = requests.get(
             f"https://api.figma.com/v1/files/{file_id}",
-            headers={"X-FIGMA-TOKEN": token})
+            headers={"X-FIGMA-TOKEN": token},
+        )
 
     except ValueError:
         messagebox.showerror(
-            "Value Error",
-            "Invalid Input. Please check your input and try again.")
+            "Value Error", "Invalid Input. Please check your input and try again."
+        )
 
     except requests.ConnectionError:
         messagebox.showerror(
-            "No Connection",
-            "Tkinter Designer requires internet access to work.")
+            "No Connection", "Tkinter Designer requires internet access to work."
+        )
 
     data = response.json()
-
 
     # Getting Window Properties
 
@@ -95,22 +93,22 @@ def generate_code(token, link, output_path):
             os.mkdir(generated_dir)
 
         except FileExistsError:
-            messagebox.showinfo("File Exists",
-                                "Existing Files will be overwritten.")
+            messagebox.showinfo("File Exists", "Existing Files will be overwritten.")
 
         except PermissionError:
-            messagebox.showerror("Permission Error",
-                                 "Change directory or directory permissions.")
-            
+            messagebox.showerror(
+                "Permission Error", "Change directory or directory permissions."
+            )
+
     except KeyError:
         messagebox.showerror(
-            "Error",
-            "Invalid Input. Please check your input and try again.")
+            "Error", "Invalid Input. Please check your input and try again."
+        )
 
     except IndexError:
         messagebox.showerror(
-            "Error",
-            "Invalid design file. Does your file contain a Frame?")
+            "Error", "Invalid design file. Does your file contain a Frame?"
+        )
 
     window_width, window_height = get_dimensions(fig_window)
 
@@ -121,24 +119,26 @@ def generate_code(token, link, output_path):
         print(e)
         window_bg_hex = "#FFFFFF"
 
-
     # Creating Window
 
-    lines.extend([f'\nwindow.geometry("{window_width}x{window_height}")',
-                  f'window.configure(bg = "{window_bg_hex}")',
-                  'canvas = Canvas(',
-                  '    window,',
-                  f'    bg = "{window_bg_hex}",',
-                  f'    height = {window_height},',
-                  f'    width = {window_width},',
-                  '    bd = 0,',
-                  '    highlightthickness = 0,',
-                  '    relief = "ridge")',
-                  'canvas.place(x = 0, y = 0)\n'])
-
+    lines.extend(
+        [
+            f'\nwindow.geometry("{window_width}x{window_height}")',
+            f'window.configure(bg = "{window_bg_hex}")',
+            "canvas = Canvas(",
+            "    window,",
+            f'    bg = "{window_bg_hex}",',
+            f"    height = {window_height},",
+            f"    width = {window_width},",
+            "    bd = 0,",
+            "    highlightthickness = 0,",
+            '    relief = "ridge")',
+            "canvas.place(x = 0, y = 0)\n",
+        ]
+    )
 
     # Getting Elements inside Window
-    
+
     window_elements = fig_window["children"]
 
     btn_count = 0
@@ -151,37 +151,64 @@ def generate_code(token, link, output_path):
             x, y = get_coordinates(element)
             element_color = get_color(element)
 
-            lines.extend(['\ncanvas.create_rectangle(',
-                          f'    {x}, {y}, {x}+{width}, {y}+{height},',
-                          f'    fill = "{element_color}",',
-                          '    outline = "")\n'])
+            lines.extend(
+                [
+                    "\ncanvas.create_rectangle(",
+                    f"    {x}, {y}, {x}+{width}, {y}+{height},",
+                    f'    fill = "{element_color}",',
+                    '    outline = "")\n',
+                ]
+            )
 
         elif element["name"] == "Button":
             width, height = get_dimensions(element)
             x, y = get_coordinates(element)
-            item_id = element["id"]
+
+            # Getting button background images
+            item_id = element["children"][0]["id"]
+            print(item_id)
 
             response = requests.get(
                 f"https://api.figma.com/v1/images/{file_id}?ids={item_id}",
-                headers={"X-FIGMA-TOKEN": f"{token}"})
+                headers={"X-FIGMA-TOKEN": f"{token}"},
+            )
 
             image_link = requests.get(response.json()["images"][item_id])
 
-            with open(f"{generated_dir}img{btn_count}.png", "wb") as file:
+            with open(f"{generated_dir}button_img_{btn_count}.png", "wb") as file:
                 file.write(image_link.content)
 
-            lines.extend([
-                f'img{btn_count} = PhotoImage(file = f"img{btn_count}.png")',
-                f'b{btn_count} = Button(',
-                f'    image = img{btn_count},',
-                '    borderwidth = 0,',
-                '    highlightthickness = 0,',
-                '    command = btn_clicked,',
-                '    relief = "flat")\n',
-                f'b{btn_count}.place(',
-                f'    x = {x}, y = {y},',
-                f'    width = {width},',
-                f'    height = {height})\n'])
+            # Getting Button text
+            try:
+                button_text = element["children"][len(element["children"]) - 1]["characters"]
+            except:
+                button_text = ""
+                button_text_color = "white"
+            else:
+                button_text_color = get_color(element["children"][len(element["children"]) - 1])
+                font, fontSize = get_text_properties(element["children"][len(element["children"]) - 1])
+
+            lines.extend(
+                [
+                    f'button_img_{btn_count} = PhotoImage(file = f"button_img_{btn_count}.png")',
+                    f"button_text_font_{btn_count} = font.Font(family='{font}', size=int({fontSize}))",
+                    f"b{btn_count} = Button(",
+                    f"    image = button_img_{btn_count},",
+                    f"    text = '{button_text}',",
+                    "    compound = 'center',",
+                    f"    fg = '{button_text_color}',",
+                    f"    font = button_text_font_{btn_count},",
+                    "    borderwidth = 0,",
+                    "    highlightthickness = 0,",
+                    "    command = btn_clicked,",
+                    "    relief = 'flat')\n",
+                    f"b{btn_count}.place(",
+                    f"    x = {x}, y = {y},",
+                    f"    width = {width},",
+                    f"    height = {height})\n",
+
+                ]
+            )
 
             btn_count += 1
 
@@ -196,17 +223,18 @@ def generate_code(token, link, output_path):
 
             text = text.replace("\n", "\\n")
 
-            lines.extend([f'canvas.create_text(',
-                          f'    {x}, {y},',
-                          f'    text = "{text}",',
-                          f'    fill = "{color}",',
-                          f'    font = ("{font}", int({fontSize})))\n'])
+            lines.extend(
+                [
+                    f"canvas.create_text(",
+                    f"    {x}, {y},",
+                    f'    text = "{text}",',
+                    f'    fill = "{color}",',
+                    f'    font = ("{font}", int({fontSize})))\n',
+                ]
+            )
 
         elif element["name"] in ("TextBox", "TextArea"):
-            element_types = {
-                "TextArea": "Text",
-                "TextBox": "Entry"
-            }
+            element_types = {"TextArea": "Text", "TextBox": "Entry"}
 
             width, height = get_dimensions(element)
             x, y = get_coordinates(element)
@@ -217,22 +245,25 @@ def generate_code(token, link, output_path):
 
             response = requests.get(
                 f"https://api.figma.com/v1/images/{file_id}?ids={item_id}",
-                headers={"X-FIGMA-TOKEN": f"{token}"})
+                headers={"X-FIGMA-TOKEN": f"{token}"},
+            )
 
             image_link = requests.get(response.json()["images"][item_id])
 
             with open(
-                    f"{generated_dir}img_textBox{text_entry_count}.png",
-                    "wb"
-                    ) as file:
+                f"{generated_dir}img_textBox{text_entry_count}.png", "wb"
+            ) as file:
                 file.write(image_link.content)
 
-            lines.extend([f'entry{text_entry_count}_img = PhotoImage('
-                          f'file = f"img_textBox{text_entry_count}.png")',
-                          f'entry{text_entry_count}_bg = '
-                          'canvas.create_image(',
-                          f'    {x}, {y},',
-                          f'    image = entry{text_entry_count}_img)\n'])
+            lines.extend(
+                [
+                    f"entry{text_entry_count}_img = PhotoImage("
+                    f'file = f"img_textBox{text_entry_count}.png")',
+                    f"entry{text_entry_count}_bg = " "canvas.create_image(",
+                    f"    {x}, {y},",
+                    f"    image = entry{text_entry_count}_img)\n",
+                ]
+            )
 
             try:
                 corner_radius = element["cornerRadius"]
@@ -249,17 +280,21 @@ def generate_code(token, link, output_path):
             x, y = get_coordinates(element)
             x = x + corner_radius
 
-            lines.extend([f'entry{text_entry_count} = '
-                          f'{element_types[element["name"]]}(',
-                          '    bd = 0,',
-                          f'    bg = "{bg}",',
-                          '    highlightthickness = 0)\n',
-                          f'entry{text_entry_count}.place(',
-                          f'    x = {x}, y = {y},',
-                          f'    width = {reduced_width},',
-                          f'    height = {reduced_height})\n'])
+            lines.extend(
+                [
+                    f"entry{text_entry_count} = " f'{element_types[element["name"]]}(',
+                    "    bd = 0,",
+                    f'    bg = "{bg}",',
+                    "    highlightthickness = 0)\n",
+                    f"entry{text_entry_count}.place(",
+                    f"    x = {x}, y = {y},",
+                    f"    width = {reduced_width},",
+                    f"    height = {reduced_height})\n",
+                ]
+            )
 
             text_entry_count += 1
+
 
         elif element["name"] == "Background":
             width, height = get_dimensions(element)
@@ -270,26 +305,29 @@ def generate_code(token, link, output_path):
             response = requests.get(
                 f"https://api.figma.com/v1/images/{file_id}"
                 f"?ids={item_id}&use_absolute_bounds=true",
-                headers={"X-FIGMA-TOKEN": f"{token}"})
+                headers={"X-FIGMA-TOKEN": f"{token}"},
+            )
 
             image_link = requests.get(response.json()["images"][item_id])
 
             with open(f"{generated_dir}background.png", "wb") as file:
                 file.write(image_link.content)
 
-            lines.extend(['background_img = PhotoImage('
-                          'file = f"background.png")',
-                          'background = canvas.create_image(',
-                          f'    {x}, {y},',
-                          f'    image=background_img)\n'])
-
+            lines.extend(
+                [
+                    "background_img = PhotoImage(" 'file = f"background.png")',
+                    "background = canvas.create_image(",
+                    f"    {x}, {y},",
+                    f"    image=background_img)\n",
+                ]
+            )
 
     # Adding Generated Code to window.py
 
-    lines.extend(['window.resizable(False, False)', 'window.mainloop()'])
+    lines.extend(["window.resizable(False, False)", "window.mainloop()"])
     final_code = [line + "\n" for line in lines]
 
-    with open(f"{generated_dir}window.py", 'w') as py_file:
+    with open(f"{generated_dir}window.py", "w") as py_file:
         py_file.writelines(final_code)
 
     messagebox.showinfo("Success", "Files created successfully!")
--- tkinter_designer.py
+++ tkinter_designer.py
@@ -1,49 +1,48 @@
-from tkinter import *
-from tkinter import filedialog, messagebox
+# Required in order to add data files to Windows executable
+import os
+import sys
 import backend
 import webbrowser
+from tkinter import *
+from tkinter import filedialog, messagebox
 
-# Required in order to add data files to Windows executable
-import sys, os
-path = getattr(sys, '_MEIPASS', os.getcwd())
+path = getattr(sys, "_MEIPASS", os.getcwd())
 os.chdir(path)
 
 
 def btn_clicked():
     token = token_entry.get()
     URL = URL_entry.get()
+    output_path = path_entry.get()
 
     if not token:
-        messagebox.showerror(title="Empty Fields",
-                             message="Please enter Token")
+        messagebox.showerror(title="Empty Fields", message="Please enter Token")
 
     elif not URL:
-        messagebox.showerror(title="Empty Fields",
-                             message="Please enter URL")
+        messagebox.showerror(title="Empty Fields", message="Please enter URL")
 
     elif not output_path:
-        messagebox.showerror(title="invalid path",
-                             message="Enter a valid output path")
+        messagebox.showerror(title="invalid path", message="Enter a valid output path")
 
     else:
-        backend.generate_code(token,URL, output_path)
+        backend.generate_code(token, URL, output_path)
 
-def select_path(event):
-    global output_path
 
-    # window.withdraw()
-    output_path = filedialog.askdirectory()
+def select_path():
+    # global output_path
+    # output_path = filedialog.askdirectory()
     path_entry.delete(0, END)
-    path_entry.insert(0, output_path)
-    # window.deiconify()
+    path_entry.insert(0, filedialog.askdirectory())
+
 
 def know_more_clicked(event):
     url = "https://github.com/ParthJadhav/Tkinter-Designer/blob/master/instructions.md"
     webbrowser.open_new(url)
 
+
 def make_label(master, x, y, h, w, *args, **kwargs):
     f = Frame(master, height=h, width=w)
-    f.pack_propagate(0) # don't shrink
+    f.pack_propagate(0)  # don't shrink
     f.place(x=x, y=y)
 
     label = Label(f, *args, **kwargs)
@@ -51,64 +50,121 @@ def make_label(master, x, y, h, w, *args, **kwargs):
 
     return label
 
+
 window = Tk()
-logo = PhotoImage(file='images/iconbitmap.gif')
-window.call('wm', 'iconphoto', window._w, logo)
+logo = PhotoImage(file="images/iconbitmap.gif")
+window.call("wm", "iconphoto", window._w, logo)
 window.title("Tkinter Designer")
 output_path = ""
 
 window.geometry("862x519")
 window.configure(bg="#3A7FF6")
-canvas = Canvas(window,bg="#3A7FF6",height=519,width=862,bd=0, highlightthickness=0,relief="ridge")
-canvas.place(x=0,y=0)
-canvas.create_rectangle(431, 0, 431 + 431, 0 + 519, fill="#FCFCFC",outline="")
-canvas.create_rectangle(40, 160, 40 + 60, 160 + 5, fill="#FCFCFC",outline="")
+canvas = Canvas(
+    window,
+    bg="#3A7FF6",
+    height=519,
+    width=862,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge",
+)
+canvas.place(x=0, y=0)
+canvas.create_rectangle(431, 0, 431 + 431, 0 + 519, fill="#FCFCFC", outline="")
+canvas.create_rectangle(40, 160, 40 + 60, 160 + 5, fill="#FCFCFC", outline="")
 
 text_box_bg = PhotoImage(file=f"images/TextBox_Bg.png")
-token_entry_img = canvas.create_image(650.5,167.5,image=text_box_bg)
-URL_entry_img = canvas.create_image(650.5,248.5,image=text_box_bg)
-filePath_entry_img = canvas.create_image(650.5,329.5,image=text_box_bg)
+token_entry_img = canvas.create_image(650.5, 167.5, image=text_box_bg)
+URL_entry_img = canvas.create_image(650.5, 248.5, image=text_box_bg)
+filePath_entry_img = canvas.create_image(650.5, 329.5, image=text_box_bg)
 
-token_entry = Entry(bd=0,bg="#F6F7F9",highlightthickness=0)
-token_entry.place(x=490.0,y=137+25,width=321.0,height=35)
+token_entry = Entry(bd=0, bg="#F6F7F9", highlightthickness=0)
+token_entry.place(x=490.0, y=137 + 25, width=321.0, height=35)
 token_entry.focus()
 
-URL_entry = Entry(bd=0,bg="#F6F7F9",highlightthickness=0)
-URL_entry.place(x=490.0,y=218+25,width=321.0,height=35)
+URL_entry = Entry(bd=0, bg="#F6F7F9", highlightthickness=0)
+URL_entry.place(x=490.0, y=218 + 25, width=321.0, height=35)
 
-path_entry = Entry(bd=0,bg="#F6F7F9",highlightthickness=0)
-path_entry.place(x=490.0,y=299+25,width=321.0,height=35)
-path_entry.bind("<1>", select_path)
+path_entry = Entry(bd=0, bg="#F6F7F9", highlightthickness=0)
+path_entry.place(x=490.0, y=299 + 25, width=321.0, height=35)
 
+canvas.create_text(
+    519.0, 156.0, text="Token ID", fill="#515486", font=("Arial-BoldMT", int(13.0))
+)
+canvas.create_text(
+    518.5, 234.5, text="File URL", fill="#515486", font=("Arial-BoldMT", int(13.0))
+)
+canvas.create_text(
+    529.5, 315.5, text="Output Path", fill="#515486", font=("Arial-BoldMT", int(13.0))
+)
+canvas.create_text(
+    646.5, 428.5, text="Generate", fill="#FFFFFF", font=("Arial-BoldMT", int(13.0))
+)
+canvas.create_text(
+    573.5,
+    88.0,
+    text="Enter the details.",
+    fill="#515486",
+    font=("Arial-BoldMT", int(22.0)),
+)
 
-canvas.create_text(519.0,156.0,text="Token ID",fill="#515486",font=("Arial-BoldMT",int(13.0)))
-canvas.create_text(518.5,234.5,text="File URL",fill="#515486",font=("Arial-BoldMT",int(13.0)))
-canvas.create_text(529.5,315.5,text="Output Path",fill="#515486",font=("Arial-BoldMT",int(13.0)))
-canvas.create_text(646.5,428.5,text="Generate",fill="#FFFFFF",font=("Arial-BoldMT",int(13.0)))
-canvas.create_text(573.5,88.0,text="Enter the details.",fill="#515486",font=("Arial-BoldMT",int(22.0)))
+title = Label(
+    text="Welcome to Tkinter Designer",
+    bg="#3A7FF6",
+    fg="white",
+    font=("Arial-BoldMT", int(20.0)),
+)
+title.place(x=27.0, y=120.0)
 
-title = Label(text="Welcome to Tkinter Designer", bg="#3A7FF6",fg="white",font=("Arial-BoldMT",int(20.0)))
-title.place(x=27.0,y=120.0)
+info_text = Label(
+    text="Tkinter Designer uses the Figma API\n"
+    "to analyse a design file, then creates\n"
+    "the respective code and files needed\n"
+    "for your GUI.\n\n"
+    "Even this GUI was created\n"
+    "using Tkinter Designer.",
+    bg="#3A7FF6",
+    fg="white",
+    justify="left",
+    font=("Georgia", int(16.0)),
+)
 
-info_text = Label(text="Tkinter Designer uses the Figma API\n"
-                   "to analyse a design file, then creates\n"
-                   "the respective code and files needed\n"
-                   "for your GUI.\n\n"
-                   
-                   "Even this GUI was created\n"
-                   "using Tkinter Designer.",
-              bg="#3A7FF6",fg="white",justify="left",font=("Georgia",int(16.0)))
+info_text.place(x=27.0, y=200.0)
 
-info_text.place(x=27.0,y=200.0)
-
-know_more = Label(text="Click here for instructions", bg="#3A7FF6",fg="white", cursor="hand2")
-know_more.place(x=27,y=400)
-know_more.bind('<Button-1>', know_more_clicked)
+know_more = Label(
+    text="Click here for instructions", bg="#3A7FF6", fg="white", cursor="hand2"
+)
+know_more.place(x=27, y=400)
+know_more.bind("<Button-1>", know_more_clicked)
 
 generate_btn_img = PhotoImage(file="./images/generate.png")
-generate_btn = Button(image=generate_btn_img, borderwidth=0, highlightthickness=0, command=btn_clicked, relief="flat")
+
+generate_btn = Button(
+    text="Generate",
+    compound="center",
+    fg="white",
+    image=generate_btn_img,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat",
+)
 generate_btn.place(x=557, y=401, width=180, height=55)
 
+path_picker_img = PhotoImage(file = f"./images/path_picker.png")
+path_picker_button = Button(
+    image = path_picker_img,
+    text = '',
+    compound = 'center',
+    fg = 'white',
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = select_path,
+    relief = 'flat')
+
+path_picker_button.place(
+    x = 783, y = 319,
+    width = 24,
+    height = 22)
 
 window.resizable(False, False)
 window.mainloop()
