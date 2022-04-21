c280826c14a08f2fcd4f9d50aa360bd50db156b7
avivfaraj <avivfaraj4@gmail.com>
2022-04-21 10:41:59 -0400

    ran some tests

--- gui/gui.py
+++ gui/gui.py
@@ -52,6 +52,7 @@ def btn_clicked():
         return
 
     file_key = match.group(1).strip()
+    print(file_key)
     token = token.strip()
     output = Path(output_path + "/build").expanduser().resolve()
 
