a6bf76006e964b3faf2e9eb2a9574cfba964b1f5
QuillX4 <advensicaddy@gmail.com>
2023-12-17 13:32:38 +0530

    update for python 3.12.1

--- tkdesigner/utils.py
+++ tkdesigner/utils.py
@@ -20,6 +20,6 @@ def download_image(url, image_path):
     response = requests.get(url)
     content = io.BytesIO(response.content)
     im = Image.open(content)
-    im = im.resize((im.size[0] // 2, im.size[1] // 2), Image.ANTIALIAS)
+    im = im.resize((im.size[0] // 2, im.size[1] // 2), Image.LANCZOS)
     with open(image_path, "wb") as file:
         im.save(file)
